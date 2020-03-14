b9330193c69ce8ef910c78cc5f3bdcf69cdec770
Patryk Matuła <60926409+patrykmatula@users.noreply.github.com>
2020-03-14 14:27:36 +0100

    add guests 1 by 1

--- GuestList.java
+++ GuestList.java
@@ -7,16 +7,39 @@ public class GuestList {
         String[] guests = new String[10];
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1 - Display All Guests");
-        System.out.println("2 - Add Guest");
-        System.out.println("2 - Remove Guest");
-        System.out.print("Option:");
-        int option = scanner.nextInt();
+        do {
+            System.out.println();
+            System.out.println("1 - Display All Guests");
+            System.out.println("2 - Add Guest");
+            System.out.println("3 - Remove Guest");
+            System.out.println("4 - Exit");
+            System.out.print("Option:");
+            int option = scanner.nextInt();
+            System.out.println();
 
-        if (option == 1) {
-            for (int i = 0; i < guests.length; i++) {
-                System.out.println(guests[i]);
+            if (option == 1) {
+                for (int i = 0; i < guests.length; i++) {
+                    System.out.println(guests[i]);
+                }
             }
-        }
+            else if (option == 2) {
+                for (int i = 0; i < guests.length; i++) {
+                    if (guests[i] == null) {
+                        System.out. print ("Name: ");
+                        String name = scanner.next();
+                        guests [i] = name;
+                        break;
+                    }
+                }
+
+
+            }
+            else if (option == 4) {
+                break;
+            }
+        } while (true);
+
+        System.out.println("Exiting...");
+
     }
 }
