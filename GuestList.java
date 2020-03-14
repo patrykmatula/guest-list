0c8971d7efc1d5f8ffa007576988f97a9dc224d6
Patryk Matuła <60926409+patrykmatula@users.noreply.github.com>
2020-03-14 16:05:28 +0100

    make list and menu as seperate methods

--- GuestList.java
+++ GuestList.java
@@ -1,32 +1,18 @@
 import java.util.Scanner;
 
 public class GuestList {
+    static String[] guests = new String[10];
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        String[] guests = new String[10];
-        Scanner scanner = new Scanner(System.in);
-
         do {
-            System.out.println("----------------------------");
-            System.out.println("- Guests -");
-            System.out.println();
-            for (int i = 0; i < guests.length; i++) {
-                System.out.println(guests[i] == null ? "--" : guests[i]);
-            }
-            System.out.println();
-            System.out.println("----------------------------");
-            System.out.println("- Menu -");
-            System.out.println();
-            System.out.println("1 - Add Guest");
-            System.out.println("2 - Remove Guest");
-            System.out.println("3 - Exit");
+            displayGuests();
+            displayMenu();
+            
             System.out.print("Option:");
             int option = scanner.nextInt();
             System.out.println();
-
-
-
 
             if (option == 1) {
                 for (int i = 0; i < guests.length; i++) {
@@ -54,6 +40,24 @@ public class GuestList {
         } while (true);
 
         System.out.println("Exiting...");
+
+    }
+    static void displayGuests() {
+        System.out.println("----------------------------");
+        System.out.println("- Guests -");
+        System.out.println();
+        for (int i = 0; i < guests.length; i++) {
+            System.out.println(guests[i] == null ? "--" : guests[i]);
+        }
+    }
+    static void displayMenu() {
+        System.out.println();
+        System.out.println("----------------------------");
+        System.out.println("- Menu -");
+        System.out.println();
+        System.out.println("1 - Add Guest");
+        System.out.println("2 - Remove Guest");
+        System.out.println("3 - Exit");
 
     }
 }
